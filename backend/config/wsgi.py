bb6f2081abdd6e80a0b58073b247a8362022435f
Andrey <shooroop87@mail.ru>
2025-10-01 23:03:24 +0300

    v3
    v3

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -11,20 +11,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 application = get_wsgi_application()
-
-# # Простая настройка WhiteNoise
-# if settings.DEBUG:
-#     # В DEBUG режиме без кеширования
-#     application = WhiteNoise(
-#         application,
-#         root=str(BASE_DIR / "collected_static"),
-#         autorefresh=True,
-#         max_age=0,
-#     )
-# else:
-#     # В PRODUCTION режиме с коротким кешем
-#     application = WhiteNoise(
-#         application,
-#         root=str(BASE_DIR / "collected_static"),
-#         max_age=3600,  # 1 час вместо года
-#     )
