111fefe5f68ce181f2693f41b5fdf48cb609a707
Andrey <shooroop87@mail.ru>
2026-01-23 23:52:20 +0300

    v2026-01-23 v5
    v2026-01-23 v5

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -9,10 +9,11 @@ from django.urls import include, path, re_path
 from django.views.i18n import set_language
 from django.contrib.sitemaps.views import sitemap
 from django.views.generic import TemplateView
-from core.sitemaps import StaticSitemap
+from core.sitemaps import StaticSitemap, CarSitemap
 
 sitemaps = {
     'static': StaticSitemap,
+    'cars': CarSitemap,
 }
 
 # --- URLs без языкового префикса ---
--- backend/core/sitemaps.py
+++ backend/core/sitemaps.py
@@ -1,10 +1,9 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
+from .models import Car
 
 
 class StaticSitemap(Sitemap):
-    """Sitemap для статических страниц"""
-    
     protocol = "https"
     changefreq = "monthly"
     priority = 0.5
@@ -26,3 +25,18 @@ class StaticSitemap(Sitemap):
 
     def changefreq(self, item):
         return item[2]
+
+
+class CarSitemap(Sitemap):
+    protocol = "https"
+    changefreq = "weekly"
+    priority = 0.9
+
+    def items(self):
+        return Car.objects.filter(is_active=True).select_related('category')
+
+    def location(self, obj):
+        return obj.get_absolute_url()
+
+    def lastmod(self, obj):
+        return obj.created_at
